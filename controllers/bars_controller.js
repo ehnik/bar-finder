03d398993ec062338c0b6c3655bb775981638e52
Kay Ehni <kay.ehnigmail.com>
2018-01-29 16:12:27 -0800

    iterated yelp results displayed with linked css styling

--- controllers/bars_controller.js
+++ controllers/bars_controller.js
@@ -1,3 +1,30 @@
+const request = require('request')
+
 exports.bars_get = function(req, res) {
-    res.render("index");
+    res.render("index", {pageData: {results : [] }} );
 };
+
+exports.bars_post = function(req, res) {
+
+
+  if(req.body.zip){
+
+    let url = "https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972"
+
+    let options = {
+      'url': url,
+      'headers': {
+      'Authorization': 'Bearer r_-bgGtf5XsJOnljQMAUOL_Poca5ItxKlQrIrIgZsANTx_GhhspMn1onvjTE8O67W7xij0VZjuk3cc-1cna8n0gAaXSKepAHKs8aHeHQWAHiBZQICOWb1lSkco1vWnYx'
+      }
+    }
+
+    let yelp = request.get(options, (error,response,body)=>{
+      body = JSON.parse(body)
+      console.log(body.businesses.length)
+      return res.render("index", {pageData: {results : body.businesses}});
+    })
+  }
+  else{
+    return res.render("index", {pageData: {results : [] }});
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
+var bars_controller = require('../controllers/bars_controller')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nightlife' });
-});
+router.get('/', bars_controller.bars_get);
 
-
+router.post('/', bars_controller.bars_post);
 
 module.exports = router;
